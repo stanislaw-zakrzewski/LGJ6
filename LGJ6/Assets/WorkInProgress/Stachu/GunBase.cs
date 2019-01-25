562268f3f910ea145497f3540bc3ec68cb0081f2
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2019-01-25 21:32:39 +0100

    First steps in guns

---
+++ LGJ6/Assets/WorkInProgress/Stachu/GunBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBase : MonoBehaviour {
+    public GameObject bulletType;
+    public GameObject enemyArea;
+
+    private List<GameObject> activeEnemies;
+
+	void Start () {
+        activeEnemies = new List<GameObject>();
+        
+	}
+	
+	void Update () {
+		
+	}
+
+    void SetActiveEnemies()
+    {
+        //from spawner get enemy list
+    }
+}
