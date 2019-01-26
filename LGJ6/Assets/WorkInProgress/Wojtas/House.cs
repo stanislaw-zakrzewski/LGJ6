9b19589ebc9de47988134fb57e37b6fcc0258128
Florke07 <maciek.socha1@gmail.com>
2019-01-26 09:26:21 +0100

    Added Home

---
+++ LGJ6/Assets/WorkInProgress/Wojtas/House.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House : MonoBehaviour {
+
+    public float maxHealth;
+    private float health;
+
+	// Use this for initialization
+	void Start () {
+        health = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+    }
+}
